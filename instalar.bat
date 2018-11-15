db4ac7b99f5b68c4c5ee37d8750242e98393d07f
KhalO <khan.maytok@gmail.com>
2018-11-15 11:38:54 -0500

    sadas

--- instalar.bat
+++ instalar.bat
@@ -1,6 +1,9 @@
 git init
 git remote add origin https://github.com/KhanMaytok/print-ticket.git
-git stash
+git add --all
+git commit -m "juajua"
+git fetch
+git merge origin/master master
 git pull origin master
 npm install
 npm install -g pm2
