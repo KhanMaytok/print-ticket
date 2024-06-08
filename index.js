709e1e2c3cf6025d15adebe2ae81aad6a9a15ff5
KhalO <Khan.maytok@gmail.com>
2024-06-08 18:05:33 -0500

    jjj

--- index.js
+++ index.js
@@ -2385,7 +2385,7 @@ function formatEmbarkDate(nextDay, input){
     // Create a new Date object and set the date to the input date
     const currentDate = new Date(date);
 
-    if(body.next_day === 'true' || body.next_day === true){
+    if(next_day === 'true' || next_day === true){
         currentDate.setDate(currentDate.getDate() + 1);
     }
 
