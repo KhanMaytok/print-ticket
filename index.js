ce54acea0e1fb43ec47e727aeb50c1c760c0461d
KhalO <Khan.maytok@gmail.com>
2026-02-24 10:58:29 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1316,6 +1316,119 @@ app.post('/ticket/invoice/20608151771', (req, res) => { // ANGEL DIVINO BUS - 20
 
 })
 
+app.post('/ticket/invoice/20614709562', (req, res) => { // NUEVO ILUCAN - 20614709562
+    console.log(req.body);
+    let body = req.body;
+    if (typeof (body) === "string") {
+        body = JSON.parse(body);
+    }
+    printer.printImage(logo).then(function (done) {
+        printer.println(" ")
+        printer.println(" ")
+        printer.alignCenter();
+        printer.bold(true)
+        printer.println(`${body.enterprise_name}`);
+        printer.bold(false)
+        printer.println(`Calle Nicolás de Pierola 720 URB.Campodonico- Chiclayo`)
+        printer.println(`PUNTO DE EMISIÓN: ${body.seller_agency}`)
+        printer.println(`R.U.C. ${body.enterprise_ruc}`);
+	    printer.println(`Venta internet autorizados: 958842029 - Chiclayo | 954909021 - Chiclayo | 942057662 - Chiclayo`);
+        printer.println(printLines());
+
+        let invoice_type = "BOLETA DE VENTA ELECTRÓNICA"
+        if (body.enterprise_client_id !== "0") {
+            invoice_type = "FACTURA ELECTRÓNICA";
+        }
+        if (body.is_vale === true) {
+            invoice_type = "VALE";
+        }
+
+        if(body.total_letter === '---') {
+            body.total_letter = numeroALetras(parseFloat(body.total), {
+                plural: 'dólares estadounidenses',
+                singular: 'dólar estadounidense',
+                centPlural: 'centavos',
+                centSingular: 'centavo'
+            });
+        }
+
+        printer.println(`${invoice_type}`);
+        printer.setTextDoubleHeight();
+        printer.setTextDoubleWidth();
+        printer.println(`${body.serie}-${body.number}`);
+        printer.setTextNormal();
+        printer.alignLeft();
+        printer.println(`FECHA EMISION: ${body.buy_date}`);
+        printer.println(`ATENDIDO POR : ${body.seller}`);
+        printer.println(`OFICINA: ${body.seller_agency}`)
+        printer.println(printLines());
+        if (body.enterprise_client_id !== "0") {
+            printer.println(`RAZÓN SOCIAL: ${body.enterprise_client}`);
+            printer.println(`RUC         : ${body.enterprise_client_id}`);
+        }
+        printer.println(`DOC PASAJERO: ${body.dni}`);
+        printer.println(`PASAJERO    : ${body.passenger_name}`);
+        printer.println(printLines());
+        printer.alignCenter();
+        printer.bold(true);
+        printer.println(`DATOS DEL VIAJE`);
+        printer.bold(false);
+        printer.println(printLines());
+        printer.alignLeft();
+        //printer.setTextDoubleHeight();                      // Set text to double height
+        printer.println(`ORIGEN     : ${body.departure}`);
+        printer.println(`DESTINO    : ${body.arrival}`);
+        printer.println(`FECHA VIAJE: ${body.departure_date}`);
+        printer.println(`HORA VIAJE : ${body.schedule_hour}`);
+        let embark_time = (body.embark_time.date).split(' ')[1].split('.')[0];
+        embark_time = formatHourString(embark_time);
+        let embark_date = formatEmbarkDate(body.next_day, body.departure_date);
+        printer.println(`F. EMBARQUE: ${embark_date}`);
+        printer.println(`H. EMBARQUE: ${embark_time}`);
+        printer.println(`ASIENTO    : ${body.seat}`);
+        printer.println(`IMPORTE    : S/ ${body.total}`);
+        printer.setTextNormal();
+
+        printer.println(printLines());
+        printer.alignCenter();
+        printer.println(`SON: ${body.total_letter}`);
+        printer.alignLeft();
+
+        printer.println(printLines()); //----------------------------------
+        printer.bold(true);
+        const forma_pago = body.payment_type.toUpperCase() === 'EFECTIVO' ? 'CONTADO' :  body.payment_type;
+        printer.println(`FORMA DE PAGO: ${forma_pago}`);
+	printer.println(`Observaciones : ${body.observation}`);
+	printer.println(`OBSERVACIONES`);
+        printer.bold(false);
+        printer.println(printLines()); //----------------------------------
+        printer.println(`${body.invoice_footer || ''}`);
+        printer.println(`ASEGURADO CON: ${body.soat_provider}`);
+        printer.println(`POLIZA N°: ${body.soat}`);
+        printer.println("PRESENTARSE 30 MINUTOS ANTES DE LA HORA DE EMBARQUE");
+        printer.println("TODO PASAJERO TIENE DERECHO A LLEVAR 20 KILOS DE EQUIPAJE DE MANO");
+	printer.println(`"VIVE LA MAGIA DEL CARNAVAL 2026 ADQUIRIENDO TU BOLETO CON ANTICIPACION"`);
+	printer.println("TENEMOS LOS MEJORES PRECIOS VISITANOS...");
+	printer.alignCenter();
+        //printer.printQR(`${body.ticket_id}`)
+
+        if (client_data.client_data.print_bottom === true) {
+            printer.println(client_data.client_data.bottom_text)
+        }
+        printer.partialCut();
+        printer.execute(function (err) {
+            if (err) {
+                console.error(`Print failed`, err);
+            } else {
+                console.log(`Print done`);
+            }
+        });
+        printer.clear();
+        res.send('<h1>UNO SAN</h1>')
+    });
+
+})
+
 app.post('/ticket/invoice/20612365432', (req, res) => { // NUEVO SAN ANTONIO - 20612365432
     console.log(req.body);
     let body = req.body;
