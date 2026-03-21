910d6285111e9a942f972ccd88d6008e7aed0d99
KhalO <Khan.maytok@gmail.com>
2026-03-21 11:59:34 -0500

    sd

--- index.js
+++ index.js
@@ -1285,8 +1285,8 @@ app.post('/ticket/invoice/20608151771', (req, res) => { // ANGEL DIVINO BUS - 20
         printer.bold(true);
         const forma_pago = body.payment_type.toUpperCase() === 'EFECTIVO' ? 'CONTADO' :  body.payment_type;
         printer.println(`FORMA DE PAGO: ${forma_pago}`);
-        printer.println(`NRO. OPERACIÓN: ${body.operacion_code || ''}`);
-	    printer.println(`ONSERVACIONES : ${body.observation || ''}`);
+        printer.println(`NRO. OPERACIÓN: ${body.operation_code || ''}`);
+	    printer.println(`OBSERVACIONES : ${body.observation || ''}`);
 		printer.println(`MODALIDAD : ${body.service_title || ''}`);
         printer.bold(false);
         printer.println(printLines()); //----------------------------------
