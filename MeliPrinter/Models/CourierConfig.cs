c85802ea65769947b871800dbff93a93622de2ec
KhalO <Khan.maytok@gmail.com>
2026-07-25 01:57:22 -0500

    chore: Update version to 2.0.11 and remove outdated comment from CourierConfig

--- MeliPrinter/Models/CourierConfig.cs
+++ MeliPrinter/Models/CourierConfig.cs
@@ -1,7 +1,5 @@
 using System.Collections.Generic;
 
-// Small comment: This class represents the configuration settings for a courier, including title, contact information, RUC line, warnings, extra footer, and additional lines.
-
 namespace MeliPrinter.Models
 {
     public class CourierConfig
--- MeliPrinter/Program.cs
+++ MeliPrinter/Program.cs
@@ -19,7 +19,7 @@ namespace MeliPrinter
                 return;
             }
 
-            var version = "2.0.0";
+            var version = "2.0.11";
             Console.WriteLine($"[meliprinter] v{version} - Iniciando...");
 
             var host = CreateHostBuilder(args).Build();
