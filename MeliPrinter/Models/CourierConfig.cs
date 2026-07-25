ec3518d9e75d464bdb61df43291c5c941d6aa271
KhalO <Khan.maytok@gmail.com>
2026-07-25 01:52:16 -0500

    chore: Update version to 2.0.10 and add class comment for CourierConfig

--- MeliPrinter/Models/CourierConfig.cs
+++ MeliPrinter/Models/CourierConfig.cs
@@ -1,5 +1,7 @@
 using System.Collections.Generic;
 
+// Small comment: This class represents the configuration settings for a courier, including title, contact information, RUC line, warnings, extra footer, and additional lines.
+
 namespace MeliPrinter.Models
 {
     public class CourierConfig
